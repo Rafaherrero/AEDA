ee8f76551ba247390aaa71a6c33c09a5b1e22c32
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-02-20 20:24:53 +0000

    Eliminando comentarios de clase

--- P1/vector_t.hpp
+++ P1/vector_t.hpp
@@ -3,19 +3,19 @@
 class vector_t
 {
 	private:
-		TDATO* base_;	/* Puntero a clase */
-		int sz_;	//tamaño
+		TDATO* base_;
+		int sz_;
 
 	public:
-		vector_t(void);	//constructor por defecto
-		vector_t(int sz);	//constructor en el que pasaremos el tamaño
-		virtual ~vector_t(void);	//destructor
-		void resize(int sz);	//redimensionar vector
-		TDATO& operator[] (int pos);	//
-		TDATO& operator[] (int pos) const; //acceder a un elemento constante
-		int get_size(void); // conocer el tamaño del vector
+		vector_t(void);
+		vector_t(int sz);
+		virtual ~vector_t(void);
+		void resize(int sz);
+		TDATO& operator[] (int pos);
+		TDATO& operator[] (int pos) const; 
+		int get_size(void); 
 
 	private:
-		void build(int sz);	//
-		void clean(void);	//
+		void build(int sz);
+		void clean(void);
 };

18d5f4911b35deed32ef0ad6edd5dbfb67fbd4cb
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-02-22 10:55:04 +0000

    Comenzando la pila

---
+++ P1/Pila/main_stack_t.cpp
@@ -0,0 +1,8 @@
+#include "../common.hpp"
+#include "stack_t.hpp"
+
+using namespace std;
+
+int main(void)
+{
+}
---
+++ P1/Pila/stack_t.hpp
@@ -0,0 +1,10 @@
+#include "../common.hpp"
+
+class stack_t
+{
+private:
+	TDATO* base_;
+public:
+	stack_t(void);
+	~stack_t(void);
+};

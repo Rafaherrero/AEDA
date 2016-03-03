5fce7019c4e5acc270843bc289f3371019f8955b
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-03-03 18:38:52 +0000

    Añadida la pila

---
+++ P2/Pila/main_stack_t.cpp
@@ -0,0 +1,33 @@
+#include "../common.hpp"
+#include "stack_t.hpp"
+
+using namespace std;
+
+int main(void)
+{
+	stack_t A(10);
+
+	for (int i=0;i<A.size();i++){
+		A.push(i+2*i);
+	}
+
+	cout << "Elemento primero de la pila A: " << A.top() << endl;
+	cout << "Modificamos el primer elemento de la pila A poniendo un 150." << endl;
+	cout << "Elementos de la pila A: " << A.size() << endl;
+	
+	A.top()=150;
+
+	for (int i=0;i<A.size();i++){
+		cout << A.pop() << endl;
+	}
+
+	cout << "Cambiamos el tamaño de la pila a 5 elementos." << endl;
+
+	A.resize(5);
+
+	cout << "Elementos de la pila A: " << A.size() << endl;
+
+	for (int i=0;i<A.size();i++){
+		A.push(i+2*i);
+	}
+}

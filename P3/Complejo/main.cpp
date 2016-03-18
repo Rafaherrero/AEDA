f218bc3c3be2e14343de5c329b02e8d187cfdc76
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-03-18 08:41:23 +0000

    Añadida practica 3

---
+++ P3/Complejo/main.cpp
@@ -0,0 +1,8 @@
+#include "complejo.hpp"
+
+using namespace std;
+
+int main (void){
+	complejo A(2,3);
+	cout << A << endl;
+}
---
+++ P3/Racional/main.cpp
@@ -0,0 +1,12 @@
+#include "racional.hpp"
+
+using namespace std;
+
+int main(void)
+{
+	racional A(32,10);
+	cout << "Este es A: " << A << endl;
+	racional B(34,12);
+	cout << "Este es B: " << B << endl;
+	cout << "Esta es la suma de A+B: " << A+B << endl;
+}

5a0880894e06b351dc3277deb43cc0c403138367
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-05-04 23:39:51 +0100

    Incluyendo clase DNI

---
+++ P6/common.hpp
@@ -0,0 +1,5 @@
+#include <vector>
+#include <unistd.h>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
---
+++ P6/main.cpp
@@ -0,0 +1,11 @@
+//Compilar usando g++ -std=c++11 main.cpp
+
+#include "common.hpp"
+#include "dni.hpp"
+#include "arbol.hpp"
+
+using namespace std;
+
+int main(void){
+	return 0;
+}
